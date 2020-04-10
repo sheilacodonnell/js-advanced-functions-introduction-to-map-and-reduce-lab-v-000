fb024dc77bbc27f218d125533f1777d3dcf29b76
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-10 15:49:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+
+function mapToNegativize(numbers) {
+  return numbers.map(n => -n )
+}
+
+function mapToNoChange(numbers) {
+  return numbers.map(n => n)
+}
+
+function mapToDouble(numbers) {
+  return numbers.map(n => n * 2)
+}
+
+function mapToSquare(numbers) {
+  return numbers.map(n => n * n)
+}
+
+function reduceToTotal(numbers, startingPoint = 0) {
+  return numbers.reduce(
+    function(total, currentValue) {
+      return total + currentValue}, startingPoint)
+}
+
+function reduceToAllTrue(numbers) {
+    return numbers.reduce(function(total, currentValue) {
+        if (currentValue) {
+            return true
+        } else {
+            return false
+        }
+    })
+}
+
+function reduceToAnyTrue(numbers) {
+    return numbers.reduce(function(total, currentValue) {
+        if (currentValue) {
+            return true
+        } else {
+            return false
+        }
+    })
+}
